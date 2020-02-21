3e1968a76bae17be4397bdc87f303ff532734994
Logan Keller <logan.keller.2023@student.lhschools.org>
2020-02-21 12:16:50 -0500

    intelliJ commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,14 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My Name Is");
+        System.out.println("Logan");
+
+        System.out.println("Went to Cedar point");
+        System.out.println("Went Swimming");
+        System.out.println("Played Games");
+    }
+}
